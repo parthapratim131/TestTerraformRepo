661c45df80b151a2f9820a7c52b29e609f48bf69
parthapratim131 <parthapratim131@outlook.com>
2025-11-11 16:12:38 +0530

    Create providers.tf

---
+++ terraform/providers.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.4"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+  }
+}
+
+provider "local" {
+  # No configuration needed
+}
+
+provider "random" {
+  # No configuration needed
+}
